1e13b5601edc90b65a52932499e5ee66119e1608
liyanxing <321339085@qq.com>
2023-03-04 16:49:20 +0800

    底部导航

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
-<script setup>
-
-</script>
-
 <template>
   <div class="app">
-		<router-view></router-view>
-		<router-link to="/home">首页</router-link>
-		<router-link to="/favor">收藏</router-link>
-		<router-link to="/message">消息</router-link>
-		<router-link to="/order">订单</router-link>
-	</div>
+    <router-view/>
+    <tab-bar/>
+  </div>
 </template>
+
+<script setup>
+
+import TabBar from "@/components/tabbar/tabbar.vue"
+
+</script>
 
 <style scoped>
 
---
+++ src/components/tabbar/tabbar.vue
@@ -0,0 +1,86 @@
+<template>
+	<div class="tabbar">
+		<!-- <template v-for="(item,index) in tabbarData">		
+			<div class="tabbar-item" >
+				<img :src="getAssetURL(item.image)" alt="">
+				<span class="text">{{ item.text }}</span>
+			</div>
+		</template> -->
+		
+	
+		    <template v-for="(item, index) in tabbarData">
+		      <div 
+		        class="tabbar-item" 
+		        :class="{ active: currentIndex === index }"
+		        @click="itemClick(index, item)"
+		      >
+		        <img v-if="currentIndex !== index" :src="getAssetURL(item.image)" alt="">
+		        <img v-else :src="getAssetURL(item.imageActive)" alt="">
+		        <span class="text">{{ item.text }}</span>
+		      </div>
+		    </template>
+		  
+
+		<!-- <div class="tabbar-item">
+			<img src="@/assets/img/tabbar/tab_home.png" alt="">
+			<span class="text">首页</span>
+		</div>
+		<div class="tabbar-item">
+			<img src="@/assets/img/tabbar/tab_favor.png" alt="">
+			<span class="text">收藏</span>
+		</div>
+		<div class="tabbar-item">
+			<img src="@/assets/img/tabbar/tab_order.png" alt="">
+			<span class="text">订单</span>
+		</div>
+		<div class="tabbar-item">
+			<img src="@/assets/img/tabbar/tab_message.png" alt="">
+			<span class="text">消息</span>
+		</div> -->
+	</div> 
+</template>
+
+<script setup>
+	
+	import tabbarData from "@/assets/data/tabbar.js"
+	import { getAssetURL } from "@/utils/load_assets.js"
+	import { ref } from "vue";
+	import { useRouter } from "vue-router";
+	
+	const currentIndex = ref(0)
+	const router = useRouter()
+	const itemClick = (index, item) => {
+	  currentIndex.value = index
+	  router.push(item.path)
+	}
+	
+</script>
+
+<style lang="less" scoped>
+	.tabbar{
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		height: 50px;
+		display: flex;
+		border-top: 1px solid #f3f3f3;
+		.tabbar-item{
+			flex: 1;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			&.active{
+				color: var(--primary-color);
+			}
+			img{
+				width: 36px;
+			}
+			.text{
+				font-size: 12px;
+				margin-top: 2px;
+			}
+		}
+	}
+</style>
